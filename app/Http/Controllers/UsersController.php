9d5b0ca833e4344c6ebc2b765bd9eb7a5292bc50
vincesanityyy <vbustillo97@gmail.com>
2020-06-11 12:51:34 +0800

    ongoing backend added seeder

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+
+
 class UsersController extends Controller
 {
-    //
+
+    public $static_password = '720DF6C2482218518FA20FDC52D4DED7ECC043AB';
+
+    public function showOne($id){
+
+        $uid = User::finOrFail($id);
+
+        $res = User::where('id',$uid)->get();
+
+        return response()->json($res);
+    }
 }
