efb45bef734966ebb2d61d3f0277ab2105fefed5
vincesanityyy <vbustillo97@gmail.com>
2020-06-11 13:27:24 +0800

    show one and post done, backend ongoing post request using json

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,12 +11,42 @@ class UsersController extends Controller
 
     public $static_password = '720DF6C2482218518FA20FDC52D4DED7ECC043AB';
 
-    public function showOne($id){
+    public function showOne(Request $request){
+        //show one
+        $uid = User::findOrFail($request->id);
 
-        $uid = User::finOrFail($id);
-
-        $res = User::where('id',$uid)->get();
+        $res = User::where('id',1)->get();
 
         return response()->json($res);
+    }
+
+    public function updateContent(Request $request){
+
+        //will append comments field
+        $validator = \Validator::make($request->all(), [
+            'comments' => 'required|string',
+            'id' => 'required|integer'
+        ]);
+
+        $user = User::findOrFail($request->id);
+
+        if ($validator->fails()) {
+            return response()->json('Check your Inputs');
+
+        } else {
+
+           if($request->password != $this->static_password) {
+            return response()->json('Incorrect Password');
+           }
+           else{
+            $user->comments = $user->comments ." ". $request->comments;
+            $user->save();
+           }
+        }
+    }
+
+    public function updateContentUsinJson(Request $request){
+        //append commend using json
+      
     }
 }
